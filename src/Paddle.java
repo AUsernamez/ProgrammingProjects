f4d5058a44d3b9e5cd42dd9eb4b2ba893a4358f5
AUsernamez <54367938+AUsernamez@users.noreply.github.com>
2022-05-24 10:38:03 -0600

    Update Paddle.java

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,1 +1,18 @@
+class Paddle {
+float x,y,xspeed,yspeed,w,h;
+color c1;
 
+//constructor
+Paddle (float tempX, float tempY, float tempW, float tempH) {
+x=tempX;
+y=tempY;
+w=tempW;
+h=tempH;
+xspeed=0;
+yspeed=0;
+
+}
+
+
+
+}
