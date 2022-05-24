a0ae587a33eee84d7f83db5fed79fcedc025b0af
AUsernamez <54367938+AUsernamez@users.noreply.github.com>
2022-05-24 10:33:47 -0600

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -1,1 +1,25 @@
+class Ball {
+color c1;
+float x,y,xspeed,yspeed,diameter;
 
+
+// constructor
+Ball(float tempX, float tempY, float tempDiameter) {
+x=tempX;
+y=tempY;
+xspeed=0;
+yspeed=0;
+diameter=tempDiameter;
+
+}
+
+
+
+
+
+
+
+
+
+
+}
