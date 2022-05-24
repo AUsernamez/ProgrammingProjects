9ea02fbcee2de534616fe7721ae78fa62a55b106
AUsernamez <54367938+AUsernamez@users.noreply.github.com>
2022-05-24 10:37:27 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,22 @@
+Ball ball;
+Endzone ez;
+Paddle paddle;
+Button button;
+PImage photo;
 
+
+
+void setup () {
+size (900,700);
+photo = loadImage("Title.png");
+}
+
+
+
+
+void draw () {
+background(0);
+image(photo, 0,0);
+
+
+}
