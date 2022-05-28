3ffa4a105bade1cd2f944347abc143c9a3c1c0e5
AUsernamez <54367938+AUsernamez@users.noreply.github.com>
2022-05-27 22:55:36 -0600

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -1,25 +1,38 @@
 class Ball {
-color c1;
-float x,y,xspeed,yspeed,diameter;
+  color c1;
+  float x, y, xspeed, yspeed, diameter;
 
 
-// constructor
-Ball(float tempX, float tempY, float tempDiameter) {
-x=tempX;
-y=tempY;
-xspeed=0;
-yspeed=0;
-diameter=tempDiameter;
+  // constructor
+  Ball(float tempX, float tempY, float tempDiameter) {
+    x=tempX;
+    y=tempY;
+    xspeed=0;
+    yspeed=0;
+    diameter=tempDiameter;
+  }
 
-}
-
-
-
-
-
-
+  void display() {
+    ellipse(x, y, diameter, diameter);
+  }
 
 
 
-
+  void move() {
+    x=x+xspeed;
+    y=y+yspeed;
+    
+  }
+  float right() {
+    return x+diameter/2;
+  }
+  float left() {
+    return x-diameter/2;
+  }
+  float top() {
+    return y-diameter/2;
+  }
+  float bottom() {
+    return y+diameter/2;
+  }
 }
